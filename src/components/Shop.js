7672c95a094c5fb02a9a6ab1b13249ca4330db27
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-10-04 20:43:41 +0530

    basic start

---
+++ src/components/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Adidas Shoes 50 Rs</h2>
+
+        <button className="btn btn-primary mx-2">-</button>
+        Add this item to cart
+        <button className="btn btn-primary mx-2">+</button>
+
+    </div>
+  )
+}
+
+export default Shop
